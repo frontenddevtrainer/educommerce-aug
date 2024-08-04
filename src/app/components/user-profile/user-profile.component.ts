b65bc3692c53e2d5903b28b24158c22923b823b4
Trainer <admin@example.com>
2024-08-04 18:43:13 +0100

    Demo for ViewEncapsulation.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, signal, ViewEncapsulation, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'edu-user-profile',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class UserProfileComponent {
 
