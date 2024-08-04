f8b7f4423cbb593ea225590c0b2db5300a309875
Trainer <admin@example.com>
2024-08-04 17:50:38 +0100

    Update user profile to toggle menu.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'edu-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+
+  isMenuShown: boolean = false
+
+  updateMenuState(){
+    this.isMenuShown = !this.isMenuShown;
+    console.log(this.isMenuShown);
+  }
 
 }
