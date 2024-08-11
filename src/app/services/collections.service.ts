d70eb88aaf31ef27f9c94d07efc1cc034c363867
Trainer <admin@example.com>
2024-08-11 17:56:19 +0100

    API error handling

--- src/app/services/collections.service.ts
+++ src/app/services/collections.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, catchError, of, tap } from 'rxjs';
 import { ConfigService } from './config.service';
 
 export interface ICollection {
@@ -16,13 +16,25 @@ export class CollectionsService {
   _http = inject(HttpClient);
   _config = inject(ConfigService);
 
-  $listing: BehaviorSubject<ICollection[]> = new BehaviorSubject<ICollection[]>(
-    []
-  );
+  $listing: BehaviorSubject<ICollection[] | null> = new BehaviorSubject<
+    ICollection[] | null
+  >([]);
 
   getCollections() {
     this._http
       .get<ICollection[]>(this._config.API_COLLECTIONS_URL)
+      .pipe(
+        tap((response: any) => {
+          if (response.status !== 200) {
+            console.log("API Error: Status not 200")
+            throw new Error('status is not 200');
+          }
+        }),
+        catchError((error) => {
+          console.log(`API Error: ${error.message || error}`);
+          return of(null);
+        })
+      )
       .subscribe((data) => {
         this.$listing.next(data);
       });
