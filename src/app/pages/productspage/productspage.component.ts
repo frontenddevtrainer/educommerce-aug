d6052b70ebfd6c7fdb038f56c725c651a9098f69
Trainer <admin@example.com>
2024-08-18 17:56:14 +0100

    Custom capitalise pipe

--- src/app/pages/productspage/productspage.component.ts
+++ src/app/pages/productspage/productspage.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from '../../directives/highlight.directive';
+import { CapitalisePipe } from '../../pipes/capitalise.pipe';
 
 @Component({
   selector: 'edu-productspage',
   standalone: true,
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, CapitalisePipe],
   templateUrl: './productspage.component.html',
   styleUrl: './productspage.component.css'
 })
 export class ProductspageComponent {
-
-}
+  heading = "technology and cars"
+} 
