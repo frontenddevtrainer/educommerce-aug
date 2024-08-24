0ba41874f5f3a32d49c259c3a83c1ff98984e2f2
Trainer <admin@example.com>
2024-08-24 18:58:11 +0100

    Project service

--- src/app/pages/product-detail-page/product-detail-page.component.ts
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
 import { CommonModule } from '@angular/common';
+import { ProjectService } from '../../services/project.service';
 
 export interface IProduct {
   id: string;
@@ -18,13 +19,18 @@ export interface IProduct {
 export class ProductDetailPageComponent implements OnInit {
   activateRoute = inject(ActivatedRoute);
   _productService = inject(ProductsService);
+  _projectService = inject(ProjectService)
 
   product: null | IProduct = null;
 
   ngOnInit(): void {
     const { id } = this.activateRoute.snapshot.params;
-    this._productService.getProductById(id).subscribe((product) => {
-      this.product = product;
-    });
+    // this._productService.getProductById(id).subscribe((product) => {
+    //   this.product = product;
+    // });
+
+    this._projectService.getProjects().subscribe((projects)=>{
+      console.log(projects);
+    })
   }
 }
