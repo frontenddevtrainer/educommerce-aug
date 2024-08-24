a36c2eae2bc086b212e81e32cdf80d111438211a
Trainer <admin@example.com>
2024-08-24 17:03:28 +0100

    Param route and add API

--- src/app/pages/product-detail-page/product-detail-page.component.ts
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -2,6 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
 
+export interface IProduct {
+  id: string;
+  name: string;
+}
+
 @Component({
   selector: 'edu-product-detail-page',
   standalone: true,
@@ -13,9 +18,12 @@ export class ProductDetailPageComponent implements OnInit {
   activateRoute = inject(ActivatedRoute);
   _productService = inject(ProductsService);
 
+  product: null | IProduct = null;
+
   ngOnInit(): void {
     const { id } = this.activateRoute.snapshot.params;
-    this._productService.getProductById(id);
-    // subscribe to http value.
+    this._productService.getProductById(id).subscribe((product) => {
+      this.product = product;
+    });
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ConfigService } from './config.service';
+import { IProduct } from '../pages/product-detail-page/product-detail-page.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-
-  // Inject HTTP and call /products/:id
+  _http = inject(HttpClient);
+  _config = inject(ConfigService);
 
   constructor() {}
 
   getProductById(id: string) {
-    // `/products/${id}`
-    // return http get observable
+    return this._http.get<IProduct>(`${this._config.API_PRODUCT_URL}/${id}`);
   }
 }
