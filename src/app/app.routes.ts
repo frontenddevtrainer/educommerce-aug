21e33c0e5ccb843aef25053ea96d48e2aa1405da
Trainer <admin@example.com>
2024-08-24 16:19:16 +0100

    ID Parameter route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ProductspageComponent } from './pages/productspage/productspage.component';
 import { CreateCollectionPageComponent } from './pages/admin/create-collection-page/create-collection-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
 
 export const routes: Routes = [
   {
@@ -14,7 +15,10 @@ export const routes: Routes = [
     path: 'products',
     component: ProductspageComponent,
   },
-
+  {
+    path: 'products/:id',
+    component: ProductDetailPageComponent,
+  },
   {
     // /admin/create-collection
     path: 'admin/create-collection',
