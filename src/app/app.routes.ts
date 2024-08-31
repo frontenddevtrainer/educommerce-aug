9e4da3d9511b7ef3c300dc816556d1670fdfa385
Trainer <admin@example.com>
2024-08-31 17:09:32 +0100

    changes for routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { CreateCollectionPageComponent } from './pages/admin/create-collection-p
 import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
 import { authGuard } from './guards/auth.guard';
 import { adminGuard } from './guards/admin.guard';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HolidaysComponent } from './pages/holidays/holidays.component';
 
 export const routes: Routes = [
   {
@@ -20,12 +22,32 @@ export const routes: Routes = [
   {
     path: 'products/:id',
     component: ProductDetailPageComponent,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     // /admin/create-collection
     path: 'admin/create-collection',
     component: CreateCollectionPageComponent,
-    canActivate: [authGuard, adminGuard] // add adminguard here.
+    canActivate: [authGuard, adminGuard], // add adminguard here.
+  },
+  {
+    path: 'holidays-summer',
+    redirectTo: 'holidays',
+  },
+  {
+    path: 'holidays-winter',
+    redirectTo: 'holidays',
+  },
+  {
+    path: 'holidays-2024',
+    redirectTo: 'holidays',
+  },
+  {
+    path: 'holidays',
+    component: HolidaysComponent,
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
   },
 ];
